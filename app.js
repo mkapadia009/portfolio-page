45ede6e869987bb60e1f7f82db72a54db22381a8
mkapadia009 <manan.a.kapadia@gmail.com>
2020-07-02 21:38:28 +0530

    Javascript added to fetch data

---
+++ app.js
@@ -0,0 +1,7 @@
+function func(){
+    var email = document.getElementById("exampleInputEmail1").value;
+    console.log(email);
+
+    var msg = document.getElementById("textmsg").value;
+    console.log(msg);
+}
