db073c2f3f5219fdef7c1bb012f1803acb227113
mkapadia009 <manan.a.kapadia@gmail.com>
2020-07-02 21:43:48 +0530

    Email Validation

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 function func(){
-    var email = document.getElementById("exampleInputEmail1").value;
-    console.log(email);
+
+    if(document.getElementById("exampleInputEmail1").value == ""){
+        alert("Enter an email address");
+    } else {
+
+        var email = document.getElementById("exampleInputEmail1").value;
+        console.log(email);
+    }
 
     var msg = document.getElementById("textmsg").value;
     console.log(msg);
